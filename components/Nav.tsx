0257f904c73bb60253e9a4bdb6444a188c63fcca
Nomta <nomta@outlook.com>
2022-07-08 17:11:27 +0300

    styles for mobile

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -118,7 +118,7 @@ export const Nav: FC<{ user?: User }> = ({ user }) => {
                   <a
                     className={
                       activeUrl === item.link
-                        ? `block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white`
+                        ? `block py-2 pr-4 pl-3 text-white bg-slate-400 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white`
                         : 'block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"'
                     }
                     aria-current={activeUrl === item.link ? 'page' : 'false'}
